c5fd1eef40e4d66b242c3537823f82f4942d4b59
Carter Matschek <carter.matschek@gmail.com>
2022-12-15 12:24:29 -0600

    updated tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Category.findByPk(req.params.id, {include: [Product]});
+    const tagData = await Tag.findByPk(req.params.id, {include: [Product]});
 
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
